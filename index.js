b8cfac456cc835c0161b2980a01c9767eb9fdd8a
mamun-prodhan <mamunpr9@gmail.com>
2023-10-31 20:22:49 +0600

    get data based on user email

--- index.js
+++ index.js
@@ -45,8 +45,22 @@ async function run() {
       res.send(result);
     });
     // bookings
+
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
-      const bookingsData = req.body;
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
